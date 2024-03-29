4122f83580f8b9bbe40fc4df70796324dd189a88
Dothuan6 <thuann6222@gmail.com>
2024-03-29 20:04:59 +0700

    Cai dat xu ly loi

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const borrowbookRouter = require("./app/routes/borrowbook.route");
+const ApiError = require("./app/api_error");
 
 // const ApiError = require("./app/api-error");
 
@@ -10,6 +11,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/borrow", borrowbookRouter);
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Không tìm thấy đường dẫn"));
+});
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application" });
 });
