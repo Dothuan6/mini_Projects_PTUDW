fe261f8c7493754b863612e60331fedd0de969aa
Dothuan6 <thuann6222@gmail.com>
2024-04-03 21:12:17 +0700

    Xây dựng chức năng nhân viên

---
+++ client/src/views/NhanVienLog.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h1>Login nhân viên</h1>
+    <form @submit="login">
+      <label class="form-label" for="username">Username:</label>
+      <input
+        class="form-control w-25"
+        type="text"
+        id="username"
+        v-model="hoTenNhanVien"
+        required
+      />
+      <label class="form-label mt-2" for="password">Password:</label>
+      <input
+        class="form-control w-25"
+        type="password"
+        id="password"
+        v-model="password"
+        required
+      />
+      <button class="btn btn-primary my-2" type="submit">Login</button>
+    </form>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      hoTenNhanVien: "",
+      password: "",
+      success: null,
+      error: null,
+    };
+  },
+  methods: {
+    login(event) {
+      event.preventDefault();
+      axios
+        .post("/api/nhanvien/login", {
+          hoTenNhanVien: this.hoTenNhanVien,
+          password: this.password,
+        })
+        .then(async (response) => {
+          const nhanvien = response.data;
+          alert("Đăng nhập thành công");
+          sessionStorage.setItem("nhanvien", JSON.stringify(nhanvien));
+          await this.$router.push("/");
+          window.location.reload();
+        })
+        .catch((error) => {
+          alert("Sai mật khẩu hoặc tên đăng nhập");
+        });
+    },
+  },
+};
+</script>
---
+++ client/src/views/NhanVienReg.vue
@@ -0,0 +1,38 @@
+<template>
+    <div v-if="NhanVien">
+    <h4>Đăng ký nhân viên</h4>
+    <NhanVienForm
+        :NhanVien="NhanVien"
+        @submit:NhanVien="addNhanVien"
+    />
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script>
+import NhanVienForm from "@/components/NhanVienRegForm.vue";
+import NhanVienService from "@/services/nhanvien.service";
+export default {
+    components: {
+        NhanVienForm,
+    },
+    data() {
+        return {
+            NhanVien: {},
+            message: "",
+        };
+    },
+    methods: {
+        async addNhanVien(data) {
+            try {
+                await NhanVienService.create(data);
+                alert('Đăng ký thành công');
+                this.$router.push({ name: "nhanvien.login" });
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+    created() {
+    },
+};
+</script>
