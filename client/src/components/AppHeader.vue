2af7df26fb10f4c8c14c0bb0c23e8411b1ad05b4
Dothuan6 <thuann6222@gmail.com>
2024-04-03 21:11:16 +0700

    hoàn thiện header

--- client/src/components/AppHeader.vue
+++ client/src/components/AppHeader.vue
@@ -1,65 +1,86 @@
 <script>
 import InputSearch from "@/components/InputSearch.vue";
-export default{
-    components: {
-        InputSearch,
+import NhanVienLog from "@/views/NhanVienLog.vue";
+import { ref } from "vue";
+export default {
+  components: {
+    InputSearch,
+    NhanVienLog,
+  },
+  data() {
+    return {
+      isLoggedIn: false,
+    };
+  },
+  created() {
+    const nhanvien = JSON.parse(sessionStorage.getItem("nhanvien"));
+    // console.log(nhanvien.hoTenNhanVien)
+    if (nhanvien) {
+      this.isLoggedIn = true;
+    }
+    // return nhanvien?.hoTenNhanVien;
+  },
+  methods: {
+    logout() {
+      sessionStorage.removeItem("nhanvien");
+      this.isLoggedIn = false;
+      window.location.reload();
     },
-    data() {
-        return {
-            searchText: "",
-            Sach: [],
-            activeIndex: -1,
-        };
-    },
-    watch: {
-        searchText() {
-            this.activeIndex = -1;
-        },
-    },
-}
-
+  },
+  setup() {
+    const nhanvien = ref(JSON.parse(sessionStorage.getItem("nhanvien")));
+    return { nhanvien };
+  },
+};
 </script>
-
 <template>
-  <!-- <nav class="navbar navbar-expand navbar-dark bg-dark">
-        <a href="/" class="navbar-brand">Ứng dụng Quản lý sách</a>
-        <div class="mr-auto navbar-nav">
-            <li class="nav-item">
-                <router-link :to="{ name: 'muon-sach' }" class="nav-link">
-                    Sách mượn
-                    <i class="fas fa-address-book"></i>
-                </router-link>
-            </li>
-        </div>
-    </nav> -->
   <nav class="navbar navbar-expand navbar-dark bg-dark">
     <div class="container-fluid">
-      <a href="/" class="navbar-brand">Ứng dụng Quản lý sách</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
-        aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <a v-if="isLoggedIn" href="/" class="navbar-brand"
+        >Quản lý {{ nhanvien.hoTenNhanVien }}</a
+      >
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarTogglerDemo02"
+        aria-controls="navbarTogglerDemo02"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
+          <li v-if="isLoggedIn" class="nav-item">
             <router-link :to="{ name: 'book' }" class="nav-link">
-              Mượn sách
+              Sách
             </router-link>
           </li>
-          <li class="nav-item">
-            <router-link :to="{ name: 'docgia.register' }" class="nav-link">
+          <li v-if="isLoggedIn" class="nav-item">
+            <router-link :to="{ name: 'showdocgia' }" class="nav-link">
+              Độc giả
+            </router-link>
+          </li>
+          <li v-if="isLoggedIn" class="nav-item">
+            <router-link :to="{ name: 'shownxb' }" class="nav-link">
+              Nhà xuất bản
+            </router-link>
+          </li>
+          <li v-if="!isLoggedIn" class="nav-item">
+            <router-link :to="{ name: 'nhanvien.register' }" class="nav-link">
               Đăng ký
             </router-link>
           </li>
-          <li class="nav-item">
-            <router-link :to="{ name: 'docgia.login' }" class="nav-link">
+          <li v-if="!isLoggedIn" class="nav-item">
+            <router-link :to="{ name: 'nhanvien.login' }" class="nav-link">
               Đăng nhập
             </router-link>
           </li>
+          <li v-if="isLoggedIn" class="nav-item">
+            <a href="#" class="nav-link" @click="logout"> Đăng xuất </a>
+          </li>
         </ul>
-        <form class="d-flex">
-          <InputSearch v-model="searchText" />
-        </form>
       </div>
     </div>
   </nav>
